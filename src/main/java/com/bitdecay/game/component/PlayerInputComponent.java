7ea0f7bbc5d83c0e7fda8555c9916843b1a00814
Tristan Havelick <tristan@havelick.com>
2017-07-28 23:29:20 -0600

    Added keyboard input stuff

---
+++ src/main/java/com/bitdecay/game/component/PlayerInputComponent.java
@@ -0,0 +1,12 @@
+package com.bitdecay.game.component;
+
+import com.bitdecay.game.gameobject.MyGameObject;
+
+/**
+ * This component marks an object as controllable by the player.
+ */
+public class PlayerInputComponent extends AbstractComponent {
+
+    public PlayerInputComponent(MyGameObject obj){
+    }
+}
