d4c8c227c3764807a6a100f26f7058a1fcac9db4
mwingfield <mwingfie@gmail.com>
2017-07-29 17:19:02 -0600

    handle ogg in sound library

--- src/main/java/com/bitdecay/game/util/SoundLibrary.java
+++ src/main/java/com/bitdecay/game/util/SoundLibrary.java
@@ -69,7 +69,11 @@ public class SoundLibrary {
             sound = sounds.get(name);
             if (sound == null) throw new RuntimeException("Could not find configured sound: " + name + ".  You must define it in the sounds.conf file.");
             if (sound.sound == null) {
-                sound.sound = Gdx.audio.newSound(Gdx.files.classpath("sound/fx/" + name + ".wav"));
+                FileHandle fxFile = Gdx.files.classpath("sound/fx/" + name + ".wav");
+                if (!fxFile.exists()) {
+                    fxFile = Gdx.files.classpath("sound/fx/" + name + ".ogg");
+                }
+                sound.sound = Gdx.audio.newSound(fxFile);
             }
 
             return sound;
@@ -100,6 +104,7 @@ public class SoundLibrary {
             if (music.music == null) {
                 FileHandle musicFile = Gdx.files.classpath("sound/music/" + name + ".mp3");
                 if (!musicFile.exists()) musicFile = Gdx.files.classpath("sound/music/" + name + ".wav");
+                if (!musicFile.exists()) musicFile = Gdx.files.classpath("sound/music/" + name + ".ogg");
                 music.music = Gdx.audio.newMusic(musicFile);
                 musics.put(name, music);
             }
