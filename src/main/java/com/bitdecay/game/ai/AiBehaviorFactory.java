6a971373d0aa9e2b189c4bd9d0d10cde19f60ab8
Logan Moore <monday.hopscotch@gmail.com>
2017-07-30 15:31:09 -0600

    eye placement updates. follow behavior

--- src/main/java/com/bitdecay/game/ai/AiBehaviorFactory.java
+++ src/main/java/com/bitdecay/game/ai/AiBehaviorFactory.java
@@ -12,7 +12,7 @@ public class AiBehaviorFactory {
             case "TiredChase":
                 return new RotatingAiBehavior(1 + rnd.nextFloat(), Arrays.asList(new ChasePlayerAiCommand(), new WaitAiCommand()));
             case "AimAndCharge":
-                return new RotatingAiBehavior(0.5f + rnd.nextFloat(), Arrays.asList(new ChasePlayerAiCommand(), new WaitAiCommand(), new ChargeAiCommand(7)));
+                return new RotatingAiBehavior(0.5f + rnd.nextFloat(), Arrays.asList(new ChasePlayerAiCommand(), new WaitAndLooktAiCommand(), new ChargeAiCommand(7)));
             case "ClockwiseWallCrawler":
                 return new SimpleAiBehavior(new CrawlOnWallAiCommand(1));
             case "CounterClockwiseWallCrawler":
